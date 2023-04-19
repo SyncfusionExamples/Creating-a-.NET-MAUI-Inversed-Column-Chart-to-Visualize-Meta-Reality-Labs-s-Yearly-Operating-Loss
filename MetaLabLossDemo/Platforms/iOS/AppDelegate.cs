0846b29d68bafc34bdb017fd94a7ace5bfaa93b2
VimalaThirumalaiKumarSF3739 <vimala.t@syncfusion.com>
2023-04-19 19:14:38 +0530

    Updated the sample

---
+++ MetaLabLossDemo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MetaLabLossDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ MetaLabLossDemo/ViewModel/MetaLabLossDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaLabLossDemo
+{
+    public class MetaLabLossDetails
+    {
+        public MetaLabLossDetails()
+        {
+            LossDetails = new List<MetaLabLossModel>()
+            {
+            new MetaLabLossModel {Year = 2019, Loss = -4.5},
+            new MetaLabLossModel {Year = 2020, Loss = -6.6},
+            new MetaLabLossModel {Year = 2021, Loss = -10.2},
+            new MetaLabLossModel {Year = 2022, Loss = -13.7}
+            };
+        }
+
+        public List<MetaLabLossModel> LossDetails { get; set; }
+    }
+}
