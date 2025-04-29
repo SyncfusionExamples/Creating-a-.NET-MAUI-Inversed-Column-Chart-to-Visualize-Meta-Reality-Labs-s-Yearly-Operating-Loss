3a943d7d2250ca912998bc5b7e3cd8fdebd96d03
KompelliSravanSyncfusion <kompellisravankumark@syncfusion.com>
2025-04-29 14:13:05 +0530

    Updated the sample with latest version Net 9

--- MetaLabLossDemo/Platforms/MacCatalyst/AppDelegate.cs
+++ MetaLabLossDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using Foundation;
 
-namespace MetaLabLossDemo;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+namespace MetaLabLossDemo
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
